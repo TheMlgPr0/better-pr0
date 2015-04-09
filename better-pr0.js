5e46fe1f29d0201f1727b028af284700557de8fe
TheMlgPr0 <themlgpr0@outlook.de>
2015-04-09 03:11:59 +0200

    Update better-pr0.js

--- better-pr0.js
+++ better-pr0.js
@@ -1,1 +1,10 @@
-
+// ==UserScript==
+// @name		better-pr0
+// @namespace	https://github.com/TheMlgPr0/better-pr0
+// @author		TheMlgPr0
+// @description	Naja was macht das ding wohl?
+// @include		http://pr0gramm.com/*
+// @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
+// @version		0.0.1
+// @require		http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
+// ==/UserScript==
